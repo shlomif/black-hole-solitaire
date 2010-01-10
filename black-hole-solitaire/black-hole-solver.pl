257272b86d4110d9cf8c06b77a8528527a3c0d00
shlomif <shlomif@b384bcd7-cfd4-0310-aca0-d78b80f7b91b>
2010-01-10 16:16:27 +0000

    Seems to work.

--- black-hole-solitaire/black-hole-solver.pl
+++ black-hole-solitaire/black-hole-solver.pl
@@ -32,7 +32,7 @@ else
     die "Could not match first foundation line!";
 }
 
-my @board_cards = map { [split/\s+/, $l]} @lines;
+my @board_cards = map { [split/\s+/, $_]} @lines;
 my @board_values = map { [map { get_rank($_) } @$_ ] } @board_cards;
 
 my $init_state = "";
@@ -48,7 +48,45 @@ my %positions = ($init_state => { prev => undef(), });
 
 my @queue = ($init_state);
 
-while (my $next_state = pop(@queue))
+my %is_good_diff = (map { $_ => 1 } (1, $#ranks));
+
+while (my $state = pop(@queue))
 {
-    
+    # The foundation
+    my $fnd = vec($state, 0, 8);
+    my $num_empty = 0;
+
+    # my @debug_pos;
+    foreach my $col_idx (0 .. $#board_values)
+    {
+        my $pos = vec($state, 4+$col_idx, 2);
+        # push @debug_pos, $pos;
+        if ($pos)
+        {
+            my $card = $board_values[$col_idx][$pos-1];
+            if (exists($is_good_diff{
+                ($card - $fnd) % scalar(@ranks)
+            }))
+            {
+                my $next_s = $state;
+                vec($next_s, 0, 8) = $card;
+                vec($next_s, 4+$col_idx, 2)--;
+                if (! exists($positions{$next_s}))
+                {
+                    $positions{$next_s} = { prev => $state, move => $col_idx };
+                    push(@queue, $next_s);
+                }
+            }
+        }
+        else
+        {
+            $num_empty++;
+        }
+    }
+    # print "Checking ", join(",", @debug_pos), "\n";
+    if ($num_empty == @board_values)
+    {
+        print "Reached a final solution!\n";
+        exit(0);
+    }
 }
