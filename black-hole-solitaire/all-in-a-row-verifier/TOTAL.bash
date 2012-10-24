754a2d26d15624a953ddb3d2008b14eaaac68853
Shlomi Fish <shlomif@shlomifish.org>
2012-10-24 11:40:38 +0200

    .

--- black-hole-solitaire/all-in-a-row-verifier/TOTAL.bash
+++ black-hole-solitaire/all-in-a-row-verifier/TOTAL.bash
@@ -3,11 +3,16 @@ function solve()
     deal_idx="$1"
     shift
 
+    echo "Solving deal $deal_idx"
+
     deal_fn="$deal_idx.all_in_a_row.board"
     sol_fn="$deal_idx.all_in_a_row.sol"
     make_pysol_freecell_board.py -t "$deal_idx" all_in_a_row > "$deal_fn"
     if all-in-a-row-solve "$deal_fn" > "$sol_fn" ; then
-        perl verify_all_in_a_row.pl "$deal_fn" "$sol_fn"
+        if ! perl verify_all_in_a_row.pl "$deal_fn" "$sol_fn" ; then
+            echo "Error in solution in deal No. $deal_idx"
+            exit -1
+        fi
     else
         echo "Unsolved";
     fi
