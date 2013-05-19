56ed9a4bc972208efd007e41f32304c2aab5f1c0
Shlomi Fish <shlomif@shlomifish.org>
2013-05-19 19:39:15 +0300

    Add the my-rank-reach script.

--- black-hole-solitaire/c-solver/scripts/my-rank-reach-test.pl
+++ black-hole-solitaire/c-solver/scripts/my-rank-reach-test.pl
@@ -5,6 +5,8 @@ use warnings;
 
 use IO::All;
 
+use Games::Solitaire::Verify::Card;
+
 use lib './t/lib';
 
 use Games::Solitaire::BlackHole::RankReachPrune::PP;
@@ -12,6 +14,11 @@ use Games::Solitaire::BlackHole::RankReachPrune::XS;
 
 my $SUCCESS = $Games::Solitaire::BlackHole::RankReachPrune::PP::SUCCESS;
 my $NOT_REACHABLE = $Games::Solitaire::BlackHole::RankReachPrune::PP::NOT_REACHABLE;
+
+sub _r
+{
+    return Games::Solitaire::Verify::Card->calc_rank(shift)-1;
+}
 
 # TEST:$b=2;
 my @backend_specs =
@@ -50,6 +57,16 @@ sub _verdict_to_s
     );
 }
 
+my $filename = shift(@ARGV);
+
+my @states = (scalar( io->file($filename)->slurp ) =~
+    /\[START BOARD\]\n(.*?)\n\[END BOARD\]/gms);
+
+if (!@states)
+{
+    die "No states found.";
+}
+
 {
     my $backend;
 
@@ -86,6 +103,31 @@ sub _verdict_to_s
     foreach my $backend_var (@backend_specs)
     {
         $backend = $backend_var;
+
+        foreach my $s (@states)
+        {
+            my $rank_re = qr/[A23456789TJQK]/;
+
+            my $new_s = $s;
+
+            if ($new_s =~ s/^Foundations: ($rank_re)//ms)
+            {
+                my $foundation = _r($1);
+
+                my @ranks = $new_s =~ /($rank_re)/gms;
+                my @rank_counts = ((0) x 13);
+                foreach my $r (@ranks)
+                {
+                    $rank_counts[_r($r)]++;
+                }
+
+                if ($backend->{backend}->($foundation, \@rank_counts) != $SUCCESS)
+                {
+                    die "Failed with state: <<<\n$s\n>>>\n and backend $backend->{id}\n";
+                }
+            }
+        }
     }
 }
+print "All states successful with all backends.\n";
 
