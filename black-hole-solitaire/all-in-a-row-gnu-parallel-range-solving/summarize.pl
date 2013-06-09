7747dcf23d513979ae3cb21445ad10996838e078
Shlomi Fish <shlomif@shlomifish.org>
2013-06-09 11:15:58 +0300

    Extract a function.

--- black-hole-solitaire/all-in-a-row-gnu-parallel-range-solving/summarize.pl
+++ black-hole-solitaire/all-in-a-row-gnu-parallel-range-solving/summarize.pl
@@ -34,34 +34,40 @@ if (not (($game eq 'black_hole') || ($game eq 'all_in_a_row')))
     die "Invalid game type";
 }
 
-my $board_idx = shift(@ARGV);
-
-my $basename = "$board_idx.summary";
-my $final_fn = "$bhs_dir/summaries/$basename";
-
-if (-e $final_fn)
+sub process_board_idx
 {
-    exit(0);
-}
+    my $board_idx = shift;
 
-my @args;
+    my $basename = "$board_idx.summary";
+    my $final_fn = "$bhs_dir/summaries/$basename";
 
-if ($is_prune)
-{
-    push @args, (qw(--rank-reach-prune));
-}
+    if (-e $final_fn)
+    {
+        exit(0);
+    }
 
-my $text = `make_pysol_freecell_board.py -F -t $board_idx $game | black-hole-solve --game $game @args -`;
+    my @args;
 
-my ($verdict, $checked, $gen) = ($text =~ m{(Solved|Unsolved).*?^Total number of states checked is (\d+)\..*?^This scan generated (\d+) states\.}ms);
+    if ($is_prune)
+    {
+        push @args, (qw(--rank-reach-prune));
+    }
 
-my $v = substr($verdict, 0, 1);
+    my $text = `make_pysol_freecell_board.py -F -t $board_idx $game | black-hole-solve --game $game @args -`;
+
+    my ($verdict, $checked, $gen) = ($text =~ m{(Solved|Unsolved).*?^Total number of states checked is (\d+)\..*?^This scan generated (\d+) states\.}ms);
+
+    my $v = substr($verdict, 0, 1);
 
 # We are using a temporary place and moving so it will be atomic.
-my $temp_fn = "$bhs_dir/temp-summaries/$basename";
-open my $out_fh, '>', $temp_fn;
-print {$out_fh} "$board_idx\t$v\t$checked\t$gen\n";
-close ($out_fh);
+    my $temp_fn = "$bhs_dir/temp-summaries/$basename";
+    open my $out_fh, '>', $temp_fn;
+    print {$out_fh} "$board_idx\t$v\t$checked\t$gen\n";
+    close ($out_fh);
 
-copy ($temp_fn, $final_fn);
-unlink ($temp_fn);
+    copy ($temp_fn, $final_fn);
+    unlink ($temp_fn);
+}
+
+my $board_idx = shift(@ARGV);
+process_board_idx($board_idx);
