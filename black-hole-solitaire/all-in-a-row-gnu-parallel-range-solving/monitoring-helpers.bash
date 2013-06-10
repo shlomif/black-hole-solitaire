5ea69154a7bb8fa42e6a2fa51387e342e4ee583d
Shlomi Fish <shlomif@shlomifish.org>
2013-06-10 17:29:40 +0300

    Add a command to upload.

--- black-hole-solitaire/all-in-a-row-gnu-parallel-range-solving/monitoring-helpers.bash
+++ black-hole-solitaire/all-in-a-row-gnu-parallel-range-solving/monitoring-helpers.bash
@@ -3,6 +3,7 @@ unalias l
 l() { ls summaries/ ; }
 c() { perl collect.pl ; }
 t() { tail -1 SUMMARY.txt ; }
+u() { myrsync SUMMARY.txt lap:Arcs/ ; myrsync SUMMARY.txt hostgator: ; }
 f1() { perl -lane '/([US])/ && print $1'; }
 f2() { perl -lane '$F[1] eq $ENV{V} and print $F[2]'; }
 report() { local w="$1" ; shift; (export V="$w" ; paste <(f2 < SUMMARY-no-prune.txt ) <(f2 < SUMMARY-prune.txt ) > iters-"$V".txt ) ; }
