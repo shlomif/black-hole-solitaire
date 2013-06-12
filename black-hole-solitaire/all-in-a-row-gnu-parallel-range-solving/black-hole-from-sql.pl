6e33fc667c490bb84262307f3f95a2d53dcd4344
Shlomi Fish <shlomif@shlomifish.org>
2013-06-12 21:57:36 +0300

    Add black-hole-from-sql.

---
+++ black-hole-solitaire/all-in-a-row-gnu-parallel-range-solving/black-hole-from-sql.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use DBIx::Simple;
+use IO::All;
+
+my $db = DBIx::Simple->connect("dbi:SQLite:dbname=./bhs.sqlite3");
+
+my $fh = io->file("./SUMMARY-no-prune.txt");
+
+{
+    my $result = $db->query("SELECT idx, status, num_checked, num_generated FROM bhs_runs");
+
+    $result->bind(my ($idx, $status, $num_checked, $num_generated));
+
+    while ($result->fetch)
+    {
+        $fh->append(join("\t", ($idx, $status, $num_checked, $num_generated)), "\n");
+    }
+}
