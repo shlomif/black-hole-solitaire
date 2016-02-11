110b4700b89b17127a027549b773bed0f9d42e07
Shlomi Fish <shlomif@shlomifish.org>
2016-02-11 16:58:57 +0200

    Add a test for the CPAN module.

---
+++ black-hole-solitaire/CI-testing/continuous-integration-testing.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+sub do_system
+{
+    my ($args) = @_;
+
+    my $cmd = $args->{cmd};
+    print "Running [@$cmd]";
+    if (system(@$cmd))
+    {
+        die "Running [@$cmd] failed!";
+    }
+}
+
+do_system({cmd => ["cd black-hole-solitaire/ && mkdir B && cd B && ../c-solver/Tatzer && make && $^X ../c-solver/run-tests.pl"]});
+
+do_system({cmd => ["cd black-hole-solitaire/Games-Solitaire-BlackHole-Solver/ && $^X Build.PL && ./Build && ./Build test && ./Build disttest"]});
