1ba7d41d990a75edc3bde34955b6ad233235126e
Shlomi Fish <shlomif@shlomifish.org>
2018-03-19 03:50:50 +0200

    fixes and cleanups.

--- black-hole-solitaire/benchmark/benchmark-c.bash
+++ black-hole-solitaire/benchmark/benchmark-c.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-export PATH="/home/shlomif/apps/test/bhs/bin/:$PATH"
+export PATH="$HOME/apps/test/bhs/bin/:$PATH"
     (
-        seq 1 20
+        seq 1 100
     ) |
     (
         while read T ; do
             echo "$T"
             make_pysol_freecell_board.py -F -t "$T" black_hole |
-            black-hole-solve - > "$T".results.txt
+            black-hole-solve --game black_hole - > "$T".results.txt
         done
     ) 2>&1 | tee -a black_hole_range_LOG
