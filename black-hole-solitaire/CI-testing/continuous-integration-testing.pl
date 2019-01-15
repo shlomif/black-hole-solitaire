13fbf88d0fb20adf306c0a260c1ec3cac65c9b3c
Shlomi Fish <shlomif@shlomifish.org>
2019-01-15 16:25:29 +0200

    [ci] remove verbosity in tests

--- black-hole-solitaire/CI-testing/continuous-integration-testing.pl
+++ black-hole-solitaire/CI-testing/continuous-integration-testing.pl
@@ -12,24 +12,36 @@ sub do_system
 
     my $cmd = $args->{cmd};
     print "Running [@$cmd]";
-    if (system(@$cmd))
+    if ( system(@$cmd) )
     {
         die "Running [@$cmd] failed!";
     }
 }
 
-my $IS_WIN = ($^O eq "MSWin32");
-my $SEP = $IS_WIN ? "\\" : '/';
-my $MAKE = $IS_WIN ? 'gmake' : 'make';
+my $IS_WIN = ( $^O eq "MSWin32" );
+my $SEP    = $IS_WIN ? "\\" : '/';
+my $MAKE   = $IS_WIN ? 'gmake' : 'make';
 
 my $cmake_gen;
-GetOptions(
-    'gen=s' => \$cmake_gen,
-) or die 'Wrong options';
+GetOptions( 'gen=s' => \$cmake_gen, )
+    or die 'Wrong options';
 
 local $ENV{RUN_TESTS_VERBOSE} = 1;
-local $ENV{HARNESS_VERBOSE} = 1;
 
-do_system({cmd => ["cd black-hole-solitaire && mkdir B && cd B && $^X ..${SEP}c-solver${SEP}Tatzer " . (defined($cmake_gen) ? qq#--gen="$cmake_gen"# : "") . " && $MAKE && $^X ..${SEP}c-solver${SEP}run-tests.pl"]});
+do_system(
+    {
+        cmd => [
+"cd black-hole-solitaire && mkdir B && cd B && $^X ..${SEP}c-solver${SEP}Tatzer "
+                . ( defined($cmake_gen) ? qq#--gen="$cmake_gen"# : "" )
+                . " && $MAKE && $^X ..${SEP}c-solver${SEP}run-tests.pl"
+        ]
+    }
+);
 
-do_system({cmd => ["cd black-hole-solitaire${SEP}Games-Solitaire-BlackHole-Solver && dzil test --all"]});
+do_system(
+    {
+        cmd => [
+"cd black-hole-solitaire${SEP}Games-Solitaire-BlackHole-Solver && dzil test --all"
+        ]
+    }
+);
