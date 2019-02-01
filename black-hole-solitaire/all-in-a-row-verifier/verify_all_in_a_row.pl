53ab39ea9ae1c93d7cc034bba91e2496c6bb465b
Shlomi Fish <shlomif@shlomifish.org>
2019-02-01 19:29:58 +0200

    restore the verifier to work.

--- black-hole-solitaire/all-in-a-row-verifier/verify_all_in_a_row.pl
+++ black-hole-solitaire/all-in-a-row-verifier/verify_all_in_a_row.pl
@@ -3,17 +3,18 @@
 use strict;
 use warnings;
 
-use Games::Solitaire::Verify::All_in_a_Row;
-use IO::All;
+use lib '.';
+use Games::Solitaire::Verify::All_in_a_Row ();
+use IO::All qw/ io /;
 
-my ($board_fn, $solution_fn) = @ARGV;
+my ( $board_fn, $solution_fn ) = @ARGV;
 my $verifier = Games::Solitaire::Verify::All_in_a_Row->new(
     {
-        board_string => scalar(io->file($board_fn)->slurp()),
+        board_string => scalar( io->file($board_fn)->slurp() ),
     }
 );
 
 my $fh = io->file($solution_fn);
-$verifier->process_solution(sub { return $fh->chomp->getline() });
+$verifier->process_solution( sub { return $fh->chomp->getline() } );
 print "Solution is OK.\n";
 exit(0);
