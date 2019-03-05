5b59b518337cedbcc1e2b1d1a85cba7c63914805
Shlomi Fish <shlomif@shlomifish.org>
2019-03-05 11:28:07 +0200

    add the benchmark script

---
+++ black-hole-solitaire/scripts/golf-bench-2.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+if ! test -d boards
+then
+    mkdir -p boards
+    gen-multiple-pysol-layouts --dir boards/ --game golf --prefix golf --suffix .board seq 1 10000
+fi
+time ./multi-bhs-solver --game golf --quiet --rank-reach-prune boards/golf{1..200}.board
