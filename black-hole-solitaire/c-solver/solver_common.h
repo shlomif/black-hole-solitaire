5e31d77da2bda54ad2d0168b16269bde85af6152
Shlomi Fish <shlomif@shlomifish.org>
2019-03-05 11:54:05 +0200

    Extract a common module/library/header.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

---
+++ black-hole-solitaire/c-solver/solver_common.h
@@ -0,0 +1,58 @@
+// solver_common.h
+// Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+//
+// Distributed under terms of the Expat license.
+
+#pragma once
+
+#include "min_and_max.h"
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <black-hole-solver/bool.h>
+#include <black-hole-solver/black_hole_solver.h>
+#include "state.h"
+
+#include "config.h"
+
+#define MAX_LEN_BOARD_STRING 4092
+
+enum GAME_TYPE
+{
+    GAME__UNKNOWN = 0,
+    GAME__BH,
+    GAME__ALL,
+    GAME__GOLF
+};
+
+static inline void out_board(
+    black_hole_solver_instance_t *const solver, const bool display_boards)
+{
+    if (!display_boards)
+    {
+        return;
+    }
+
+    char board[1000];
+    if (black_hole_solver_get_current_solution_board(solver, board) ==
+        BLACK_HOLE_SOLVER__SUCCESS)
+    {
+        printf("\n[START BOARD]\n%s[END BOARD]\n\n\n", board);
+    }
+}
+
+static const char *const help_text =
+    "black-hole-solve --game {all_in_a_row|black_hole} [more options] "
+    "[/path/to/board_layout.txt]\n"
+    "\n"
+    "--help                        displays this help.\n"
+    "--max-iters [iter_count]      limit the iterations.\n"
+    "--game all_in_a_row           solve All in a Row games.\n"
+    "--game black_hole             solve Black Hole games.\n"
+    "--displays-boards             display the layout of the board at every "
+    "step.\n"
+    "--rank-reach-prune            enable the Rank Reachability Prune.\n"
+    "--iters-display-step [step]   Display a trace every certain step.\n"
+    "\n";
