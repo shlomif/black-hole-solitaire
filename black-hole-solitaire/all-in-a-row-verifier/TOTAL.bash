39944105a5367d5c061ae5026ed668f0ed8abf49
Shlomi Fish <shlomif@shlomifish.org>
2019-03-29 16:20:50 +0300

    refactor

--- black-hole-solitaire/all-in-a-row-verifier/TOTAL.bash
+++ black-hole-solitaire/all-in-a-row-verifier/TOTAL.bash
@@ -1,19 +1,22 @@
 solve()
 {
-    deal_idx="$1"
+    local deal_idx="$1"
     shift
 
     echo "Solving deal $deal_idx"
 
-    deal_fn="$deal_idx.all_in_a_row.board"
-    sol_fn="$deal_idx.all_in_a_row.sol"
-    make_pysol_freecell_board.py -t "$deal_idx" all_in_a_row > "$deal_fn"
-    if black-hole-solve --game all_in_a_row "$deal_fn" > "$sol_fn" ; then
-        if ! perl verify_all_in_a_row.pl "$deal_fn" "$sol_fn" ; then
+    local deal_fn="$deal_idx.all_in_a_row.board"
+    local sol_fn="$deal_idx.all_in_a_row.sol"
+    make_pysol_freecell_board.py -F -t "$deal_idx" all_in_a_row > "$deal_fn"
+    if black-hole-solve --game all_in_a_row "$deal_fn" > "$sol_fn"
+    then
+        if ! perl verify_all_in_a_row.pl "$deal_fn" "$sol_fn"
+        then
             echo "Error in solution in deal No. $deal_idx"
-            exit -1
+            return -1
         fi
     else
         echo "Unsolved";
     fi
+    return 0
 }
