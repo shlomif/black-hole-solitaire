414412d3037478c87ec416a8b315ab9544174ad0
Shlomi Fish <shlomif@shlomifish.org>
2019-04-16 22:16:20 +0300

    tweak-range-solve-for-multi-bhs-compat

--- black-hole-solitaire/scripts/golf-pat-perl-range-solve.bash
+++ black-hole-solitaire/scripts/golf-pat-perl-range-solve.bash
@@ -2,20 +2,20 @@ mkdir -p boards
 /home/shlomif/progs/freecell/git/fc-solve/fc-solve/source/board_gen/gen-multiple-pysol-layouts --dir boards/ --game golf --prefix golf --suffix .board seq 1 10000
 (
   perl_cmd_line="perl -Ilib bin/golf-solitaire-solve-perl"
-  c_cmd_line="./black-hole-solve --game golf --display-boards"
+  c_cmd_line="./black-hole-solve --game golf"
   cmd="$c_cmd_line"
   run()
   {
       local args="$1"
       shift
-      echo "== ] $fn $args [ =="
+      echo "[= Starting file $fn =]"
       $cmd $args "$fn"
+      echo "[= END of file $fn =]"
   }
-  for i in $(seq 1 10000)
+  for i in $(seq 1 200)
   do
       fn=boards/golf"$i".board
-      echo "== $fn =="
-      run "" || run "--queens-on-kings" || run "--wrap-ranks"
+      run "" # || run "--queens-on-kings" || run "--wrap-ranks"
       # run "--queens-on-kings"
   done
-) | timestamper | tee -a ~/golfs6.txt
+)
