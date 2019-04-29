d20d468459f95b147d2fa4cc56913af6c08caddf
Shlomi Fish <shlomif@shlomifish.org>
2019-04-29 19:48:42 +0300

    add wrapped golf.

---
+++ black-hole-solitaire/all-in-a-row-verifier/WRAP-TOTAL.bash
@@ -0,0 +1,22 @@
+solve()
+{
+    local deal_idx="$1"
+    shift
+
+    echo "Solving deal $deal_idx"
+
+    local deal_fn="$deal_idx.golf.board"
+    local sol_fn="$deal_idx.golf.sol"
+    make_pysol_freecell_board.py -F -t "$deal_idx" golf > "$deal_fn"
+    if black-hole-solve --game golf --wrap-ranks "$deal_fn" > "$sol_fn"
+    then
+        if ! perl verify_wrap_golf.pl "$deal_fn" "$sol_fn"
+        then
+            echo "Error in solution in deal No. $deal_idx"
+            return -1
+        fi
+    else
+        echo "Unsolved";
+    fi
+    return 0
+}
