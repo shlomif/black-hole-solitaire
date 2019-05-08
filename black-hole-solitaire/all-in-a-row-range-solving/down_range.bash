e059230fa422f09fe1e80f7e41a45e4b3968640b
Shlomi Fish <shlomif@shlomifish.org>
2019-05-08 15:36:17 +0300

    Remove duplicate files.

--- black-hole-solitaire/all-in-a-row-range-solving/down_range.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/progs/games/black-hole-solitaire/trunk/black-hole-solitaire/c-solver/build/:$PATH"
-perl down-range.pl 2>&1 | tee -a "$$.black-hole.LOG"
--- black-hole-solitaire/all-in-a-row-range-solving/down_range_remote.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/progs/games/black-hole-solitaire/trunk/black-hole-solitaire/c-solver/build/:$PATH"
-perl down-range-remote.pl 2>&1 | tee -a "$$.black-hole.LOG"
