adb3cfed3ed4c118feade9bab6eeafd1b871d9b2
Shlomi Fish <shlomif@shlomifish.org>
2020-01-31 15:48:42 +0200

    refactor the benchmark script and add features.

--- black-hole-solitaire/benchmark/benchmark-perl-before-and-after-switch-tasking.bash
+++ black-hole-solitaire/benchmark/benchmark-perl-before-and-after-switch-tasking.bash
@@ -1,11 +1,50 @@
 #!/usr/bin/env bash
-if ! test -d ../foo
+
+# set -e -x
+out_dir='../foo'
+if ! test -d "$out_dir"
 then
-    mkdir ../foo
-    gen-multiple-pysol-layouts --dir ../foo --ms --prefix '' --suffix .board --game black_hole seq 1 32000
+    mkdir -p "$out_dir"
+    gen-multiple-pysol-layouts --dir "$out_dir" --ms --prefix '' --suffix .board --game black_hole seq 1 32000
 fi
 
-(for i in ../foo/{1..32000}.board ; do perl -Ilib bin/black-hole-solve --seed 0 --task-name s0 --next-task --seed 1 --task-name s1 --next-task --seed 2 --task-name s2 --next-task --seed 3 --task-name s3 --prelude '200@s2,300@s1,400@s1,500@s0' --quiet "$i" ; done) | cat -n | timestamper | tee -a new.log.txt
-(for i in ../foo/{1..32000}.board ; do perl -Ilib bin/black-hole-solve --quiet "$i" ; done) | cat -n | timestamper | tee -a old.log.txt
-f() { export TITLE="$2"; cat "$1" | head -34 | perl -lanE 'say +($F[0]-($x//=$F[0]))||$ENV{TITLE}'}
-paste -d ,, <(seq 1 34) <(f old.log.txt Old) <(f new.log.txt New) | csv2chart xlsx -o foo.xlsx --exec gnumeric
+max_idx="100"
+
+log()
+{
+    local log_fn="$1"
+    shift
+    cat -n | timestamper | tee -a "$log_fn"
+}
+
+new_feature_args=(--seed 0 --task-name s0 --next-task --seed 1 --task-name s1 --next-task --seed 2 --task-name s2 --next-task --seed 3 --task-name s3 --prelude '200@s2,300@s1,400@s1,500@s0')
+old_ver_args=()
+
+run()
+{
+    local log_fn="$1"
+    shift
+    declare -a my__run_args=("${!1}")
+    shift
+    (
+        local i=1
+        while test "$i" -le "$max_idx"
+        do
+            perl -Ilib bin/black-hole-solve "${my__run_args[@]}" --quiet "$out_dir/$i.board"
+            (( ++i ))
+        done
+    ) | log "$log_fn"
+}
+
+if test -z "$SKIP"
+then
+    run "new.log.txt" new_feature_args[@]
+    run "old.log.txt" old_ver_args[@]
+fi
+
+f()
+{
+    export TITLE="$2"
+    cat "$1" | head -"$max_idx" | perl -lanE 'say +($F[0]-($x//=$F[0]))||$ENV{TITLE}'
+}
+paste -d ,, <(seq 1 "$max_idx") <(f old.log.txt Old) <(f new.log.txt New) | csv2chart xlsx -o foo.xlsx --exec gnumeric
