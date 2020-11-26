f960b1bf45e5e54436b2e3a51bf3c390394c4758
Shlomi Fish <shlomif@shlomifish.org>
2020-11-26 15:25:02 +0200

    pass python code pylint conformance

--- black-hole-solitaire/c-solver/scripts/source_filter.py
+++ black-hole-solitaire/c-solver/scripts/source_filter.py
@@ -5,22 +5,26 @@
 # Copyright © 2020 Shlomi Fish < https://www.shlomifish.org/ >
 #
 # Licensed under the terms of the MIT license.
+"""
+source_filter for removing the max_num_played feature from
+black-hole-solver's C code.
+"""
 
 import re
 import sys
 from pathlib import Path
 
 if sys.argv[1] == '--process=no_max_num_played':
-    should_process = True
+    SHOULD_PROCESS = True
 elif sys.argv[1] == '--process=none':
-    should_process = False
+    SHOULD_PROCESS = False
 else:
     raise Exception("wrong invocation")
 
 
-def _newlinify(text, m):
-    start = text.rindex("\n", 0, m.start(0)+1)
-    end = text.index("\n", m.end(0)-1, -1)
+def _newlinify(text, match_object):
+    start = text.rindex("\n", 0, match_object.start(0)+1)
+    end = text.index("\n", match_object.end(0)-1, -1)
     prefix = text[0:start] + "".join([
         x for x in text[start:end] if x == '\n'
     ])
@@ -28,9 +32,9 @@ def _newlinify(text, m):
 
 
 def _remove(text, pat):
-    m = re.search(pat, text, flags=(re.M | re.S))
-    assert m
-    return _newlinify(text, m)
+    match_object = re.search(pat, text, flags=(re.M | re.S))
+    assert match_object
+    return _newlinify(text, match_object)
 
 
 def process_black_hole_solver_h(text):
@@ -79,17 +83,18 @@ def process_solver_common_h(text):
     return _clear_all_individual_lines(text, "max_num_played")
 
 
-def wrapper(basename, cb):
+def wrapper(basename, callback):
+    """optionally filter file basename through callback"""
     src_fn = Path(sys.argv[0]).parent.parent / basename
     dest_fn = Path(".") / "generated" / basename
 
-    p = dest_fn.parent
-    if not p.exists():
-        p.mkdir(parents=True)
+    parent = dest_fn.parent
+    if not parent.exists():
+        parent.mkdir(parents=True)
     with open(dest_fn, "wt") as ofh:
         with open(src_fn, "rt") as ifh:
             text = ifh.read()
-            ofh.write(cb(text) if should_process else text)
+            ofh.write(callback(text) if SHOULD_PROCESS else text)
 
 
 wrapper(
