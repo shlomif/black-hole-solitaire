7fc5da663f8b7cb66ccaf57c12fc01d365d10e3c
Shlomi Fish <shlomif@shlomifish.org>
2022-05-27 05:48:18 +0300

    add possibly faulty script

---
+++ black-hole-solitaire/benchmark/benchmark-perl.pl
@@ -0,0 +1,90 @@
+#! /usr/bin/env perl
+#
+# Short description for benchmark-perl.pl
+#
+# Version 0.0.1
+# Copyright (C) 2022 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the MIT license.
+
+use strict;
+use warnings;
+use 5.014;
+use autodie;
+
+use Carp qw/ confess /;
+use Getopt::Long qw/ GetOptions /;
+use Path::Tiny qw/ cwd path tempdir tempfile /;
+use Docker::CLI::Wrapper::Container v0.0.4 ();
+
+sub do_system
+{
+    my ( $self, $args ) = @_;
+
+    my $cmd = $args->{cmd};
+    print "Running [@$cmd]\n";
+    if (0)    # system(@$cmd) >> 1 )
+    {
+        die "Running [@$cmd] failed!";
+    }
+
+    return;
+}
+
+sub run
+{
+    my $output_fn;
+
+    my $obj = Docker::CLI::Wrapper::Container->new(
+        { container => "rinutils--deb--test-build", sys => "debian:sid", } );
+
+    GetOptions( "output|o=s" => \$output_fn, )
+        or die "errror in cmdline args: $!";
+
+    if ( !defined($output_fn) )
+    {
+        #        die "Output filename not specified! Use the -o|--output flag!";
+    }
+    foreach my $deal ( 1 .. 20 )
+    {
+        print "$deal\n";
+
+        # $obj->do_system(
+        __PACKAGE__->do_system(
+            {
+                cmd => [
+qq#make_pysol_freecell_board.py -F -t "$deal" black_hole | black-hole-solve - > "$deal".results.txt#
+
+                ]
+            }
+        );
+    }
+
+    exit(0);
+}
+
+run();
+
+1;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+XML::Grammar::Screenplay::App::FromProto
+
+=head1 VERSION
+
+version v0.16.0
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2007 by Shlomi Fish.
+
+This is free software, licensed under:
+
+  The MIT (X11) License
+
+=cut
