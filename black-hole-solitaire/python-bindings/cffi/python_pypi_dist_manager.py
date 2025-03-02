7e2785bf425446763dd1fc98fecce38a2417ac14
Shlomi Fish <shlomif@shlomifish.org>
2025-03-02 17:35:08 +0200

    disable entry-point and sort the parameters

--- black-hole-solitaire/python-bindings/cffi/python_pypi_dist_manager.py
+++ black-hole-solitaire/python-bindings/cffi/python_pypi_dist_manager.py
@@ -11,15 +11,16 @@ from pydistman import DistManager
 dist_name = "black_hole_solver"
 
 obj = DistManager(
+    aur_email="shlomif@cpan.org",
     dist_name=dist_name,
     dist_version="0.2.3",
-    project_name="Black Hole Solver Wrapper",
-    project_short_description="Solve Golf and Black Hole solitaires",
-    release_date="2020-06-23",
-    project_year="2020",
-    aur_email="shlomif@cpan.org",
-    project_email="shlomif@cpan.org",
+    entry_point="none",
     full_name="Shlomi Fish",
     github_username="shlomif",
+    project_email="shlomif@cpan.org",
+    project_name="Black Hole Solver Wrapper",
+    project_short_description="Solve Golf and Black Hole solitaires",
+    project_year="2020",
+    release_date="2020-06-23",
 )
 obj.cli_run()
