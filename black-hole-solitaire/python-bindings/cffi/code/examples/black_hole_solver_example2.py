6c709debc309ff035f3dca66bc21e851bd4b319e
Shlomi Fish <shlomif@shlomifish.org>
2026-04-15 19:19:54 +0300

    implement CLI-configurable board deals indexes

--- black-hole-solitaire/python-bindings/cffi/code/examples/black_hole_solver_example2.py
+++ black-hole-solitaire/python-bindings/cffi/code/examples/black_hole_solver_example2.py
@@ -11,6 +11,8 @@
 A range-solver to test memory-consumption .
 '''
 
+import sys
+
 from black_hole_solver import BlackHoleSolver
 
 from pysol_cards.cards import CardRenderer
@@ -40,23 +42,36 @@ def main():
               'max_num_times = {} ; longest_idx = {} ]'.format(
                   deal_idx, max_num_times, longest_idx), flush=True)
     while True:
-        deal_idx += 1
-        if deal_idx % 20 == 0:
-            _output_progress()
-        board = make_pysol_board(deal_idx)
-        solver.read_board(
-            board=board,
-            game_type=VARIANT,
-            place_queens_on_kings=True,
-            wrap_ranks=True,
-        )
-        solver.resume_solution()
-        this_count = solver.get_num_times()
-        if max_num_times < this_count:
-            max_num_times = this_count + 0
-            longest_idx = deal_idx
-            _output_progress()
-        solver.recycle()
+        if len(sys.argv) <= 1:
+            break
+        arg = sys.argv.pop(1)
+        if arg == 'seq':
+            arg = sys.argv.pop(1)
+            start = int(arg)
+            arg = sys.argv.pop(1)
+            top = int(arg)
+            r = range(start, top + 1)
+        else:
+            start = int(arg)
+            r = range(start, start + 1)
+
+        for deal_idx in r:
+            if deal_idx % 20 == 0:
+                _output_progress()
+            board = make_pysol_board(deal_idx)
+            solver.read_board(
+                board=board,
+                game_type=VARIANT,
+                place_queens_on_kings=True,
+                wrap_ranks=True,
+            )
+            solver.resume_solution()
+            this_count = solver.get_num_times()
+            if max_num_times < this_count:
+                max_num_times = this_count + 0
+                longest_idx = deal_idx
+                _output_progress()
+            solver.recycle()
 
 
 if __name__ == "__main__":
